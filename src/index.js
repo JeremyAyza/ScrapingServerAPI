918cbde2f1547b62149a245d8080d653ebe81abf
Jeremy Ayza <AyzaJeremyx7x@gmail.com>
2024-05-23 15:46:10 -0500

    fix: :bug: Fix port error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import app from "./app.js";
+const PORT = process.env.PORT || 4000;
 // import { sequelize } from "./database/database.js";
 
 async function main() {
   // await sequelize.sync({force: false});
-  app.listen(4000);
-  console.log("Server on port 4000");
+  app.listen(PORT);
+  console.log("Server Running");
 }
 
 main();
