e173b41fc33e3a7a451adc34a91953e5a21426ed
jeremyayza <ayzajeremyx7x@gmail.com>
2024-05-24 08:54:21 -0500

    add argument no sand box

--- src/page.js
+++ src/page.js
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer"
 
 const browser = await puppeteer.launch({
     headless: true,
-    ignoreDefaultArgs: ['--disable-extensions'], // this made it work for now
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
     // executablePath: executablePath(),
 })
 const page = await browser.newPage()
