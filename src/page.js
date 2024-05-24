9a3d3c54416f9cbd737d0868f6d470af37bac1e5
jeremyayza <ayzajeremyx7x@gmail.com>
2024-05-24 09:06:12 -0500

    remove exacutable path

--- src/page.js
+++ src/page.js
@@ -2,8 +2,8 @@ import puppeteer from "puppeteer"
 
 const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: executablePath(),
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    // executablePath: executablePath(),
 })
 const page = await browser.newPage()
 export default page
