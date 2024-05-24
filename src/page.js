0504e0438f63167d9b617fcb04fa5226443c4836
jeremyayza <ayzajeremyx7x@gmail.com>
2024-05-24 08:49:24 -0500

    subida de archivos

--- src/page.js
+++ src/page.js
@@ -1,5 +1,9 @@
 import puppeteer from "puppeteer"
 
-const browser = await puppeteer.launch()
+const browser = await puppeteer.launch({
+    headless: true,
+    ignoreDefaultArgs: ['--disable-extensions'], // this made it work for now
+    // executablePath: executablePath(),
+})
 const page = await browser.newPage()
 export default page
