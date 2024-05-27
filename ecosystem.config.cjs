b76078454a8938ded33218b5d26466bd20fbbb01
Jeremy Ayza <AyzaJeremyx7x@gmail.com>
2024-05-27 11:02:45 -0500

    add ecomcismten

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,13 @@
-module.exports = {
+const config = {
     apps: [
         {
             name: 'express-app',
-            script: 'app.js', // El nombre de tu archivo principal de la aplicación
+            script: 'index.js', // El nombre de tu archivo principal de la aplicación
             instances: 3, // Número de instancias
             exec_mode: 'cluster', // Modo de ejecución: cluster
             watch: true, // Si quieres que PM2 reinicie automáticamente si los archivos cambian
-            max_memory_restart: '1G', // Reiniciar si se excede esta cantidad de memoria
         },
     ],
 };
+
+module.exports = config;
